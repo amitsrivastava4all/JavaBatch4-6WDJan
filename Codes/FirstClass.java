17dade0841873c07c8254a0b93a5f7c43fa8e487
amit4alljava <amit4alljava@gmail.com>
2017-01-22 18:32:26 +0530

    Codes

---
+++ Codes/FirstClass.java
@@ -0,0 +1,19 @@
+// Class name should be start with Capital Letter
+class SecondClass{
+// public - now main has public access and now u can access it outside the folder(Package)
+// static - static things will be  loaded in the memory when class is loaded
+// class load once so static things come once , so main has loaded only once
+// void - no return 
+// main - entry point of every java program
+// String - It is  a predefine class, and it is internally as char[]
+// String class has predefine methods e.g uppercase , length , index , replace
+// args - it is variable of type String, the type of this variable is always an array
+// String arr[] - it called command line argument
+public static void main(String arr[]){
+	System.out.println("Hello Java ");
+	// System it is a predefine class
+	// out is predefine object of PrintStream class
+	// println is a function or method of PrintStream class
+	// System.in, System.err
+}
+}
